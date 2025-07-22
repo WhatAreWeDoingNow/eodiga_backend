05f87fad145e13dbde2c03234fde4916c70a79f0
HP657 <junlee5070@gmail.com>
2025-07-22 12:54:05 +0900

    project init

---
+++ src/test/java/com/WhatAreWeDoingNow/eodiga/EodigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WhatAreWeDoingNow.eodiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
