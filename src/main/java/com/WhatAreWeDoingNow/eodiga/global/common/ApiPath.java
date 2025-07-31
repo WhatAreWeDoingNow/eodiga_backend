fb459a7b7e82bb95b54540bb2cba47a36f39d106
HP657 <junlee5070@gmail.com>
2025-07-31 16:28:28 +0900

    이벤트 엔티티 제거 및 H2디비에서 -> MySQL,로 변경

--- src/main/java/com/WhatAreWeDoingNow/eodiga/global/common/ApiPath.java
+++ src/main/java/com/WhatAreWeDoingNow/eodiga/global/common/ApiPath.java
@@ -1,24 +1,8 @@
 package com.WhatAreWeDoingNow.eodiga.global.common;
 
-
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Component
 public class ApiPath {
     public static final String version = "/v1";
     public static final String BASE_PATH = "/api" + version;
     public static final String AUTH_ROOT = BASE_PATH + "/auth";
     public static final String STORE_ROOT = BASE_PATH + "/store";
-
-    public static String H2_PATH;
-
-    @Value("${spring.datasource.h2.console.path}")
-    private String h2Path;
-
-    @PostConstruct
-    public void init() {
-        H2_PATH = h2Path;
-    }
 }
